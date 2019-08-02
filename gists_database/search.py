7201ab12af10feb31fe4ce9404775ded0b436332
chinnappa-raju <chinnappa_raju@outlook.com>
2019-08-02 02:07:50 +0000

    gists-database project done

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,14 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    sql = "select * from gists"
+    if len(kwargs) > 0:
+        sql+=" where "
+    for key in kwargs.keys():
+        if key == 'created_at':
+            sql+= " datetime({}) = datetime(:{}) ".format(key, key)
+        else:
+            sql+= " {} = :{}".format(key, key)
+    cursor = db_connection.execute(sql, kwargs)
+    gists = cursor.fetchall()
+    return [Gist(row) for row in gists]
